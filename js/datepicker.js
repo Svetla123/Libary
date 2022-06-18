fd9af8cee68a7cedb3119b6f45f0455945e4a9ad
Luka Svetlečić <72402718+Svetla123@users.noreply.github.com>
2022-06-18 13:45:09 +0200

    Add files via upload

---
+++ js/datepicker.js
@@ -0,0 +1,12 @@
+// $(function () {
+//   rome(inline_cal, { time: false });
+//   console.log(inline_cal);
+
+//   let days = document.getElementsByClassName("rd-day-body");
+//   for (const day of days) {
+//     day.addEventListener("click", function () {
+//       let date = document.getElementsByClassName("rd-day-selected");
+//       console.log(date.innerHTML);
+//     });
+//   }
+// });
